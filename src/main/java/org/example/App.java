f88aeb00e161c237faf0fcc6c3e1b742c8a91b5e
ridvmodi <ridhavgarg01@gmail.com>
2020-03-16 19:05:10 +0530

    initial commmit

---
+++ src/main/java/org/example/App.java
@@ -0,0 +1,84 @@
+package org.example;
+
+
+import java.util.AbstractList;
+import java.util.Arrays;
+import java.util.logging.*;
+class MyList<E> extends AbstractList<E>
+{
+    private static final Logger LOGGER=Logger.getLogger(App.class.getName());
+    private static final int INITIAL_CAPACITY = 10;
+    private int actionSize = 0;
+    private Object elementData[];
+    private int i=0;
+    public MyList()
+    {
+        elementData=new Object[INITIAL_CAPACITY];
+        i=0;
+    }
+
+    @SuppressWarnings("unchecked")
+    public E get(int index)
+    {
+        E x =(E)elementData[index];
+        return x;
+    }
+    public boolean add(E e)
+    {
+        elementData[i]=e;
+        i++;
+
+        if(i >= actionSize)
+        {
+            increaseListSize();
+        }
+        return true;
+    }
+    @SuppressWarnings("unchecked")
+    public E remove(int index)
+    {
+        int j;
+        Object x=elementData[index];
+        for(j=index; j< actionSize -1; j++)
+        {
+            elementData[j]=elementData[j+1];
+        }
+        i--;
+        actionSize--;
+        return (E)x;
+    }
+    public int size()
+    {
+        return i;
+    }
+    private void increaseListSize()
+    {
+        actionSize = elementData.length * 2;
+        elementData = Arrays.copyOf(elementData, actionSize);
+    }
+    public void display()
+    {
+        String s="";
+        for (int i = 0; i < this.i; i++)
+            s=s+elementData[i]+" ";
+        LOGGER.info(s+"\n");
+    }
+}
+
+public class App
+{
+    private static final Logger LOGGER=Logger.getLogger(App.class.getName());
+    public static void main(String[] args) {
+        MyList<Integer> ml = new MyList<Integer>();
+        LOGGER.severe("Executing My List\n");
+        for(int i = 0;i<15;i++)
+        {
+            ml.add(i);
+        }
+        ml.remove(3);
+        ml.remove(3);
+        LOGGER.info("Size of my List :"+ml.size()+"");
+        LOGGER.info(" List : \n");
+        ml.display();
+    }
+}
